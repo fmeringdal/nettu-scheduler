beff2e0ce6e72b74b630b4a3b646f989673ea0ca
Fredrik Meringdal <fmeringdal@hotmail.com>
2020-12-28 23:54:52 +0100

    some refactoring

---
+++ server/src/event/api.rs
@@ -0,0 +1,21 @@
+use super::usecases::create_event::create_event_controller;
+use super::usecases::delete_event::delete_event_controller;
+use super::usecases::get_event::get_event_controller;
+use super::usecases::get_event_instances::get_event_instances_controller;
+use super::usecases::update_event::update_event_controller;
+use actix_web::web;
+
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    // Hookup Routes to usecases
+    cfg.route("/events", web::post().to(create_event_controller));
+    cfg.route("/events/{event_id}", web::get().to(get_event_controller));
+    cfg.route("/events/{event_id}", web::put().to(update_event_controller));
+    cfg.route(
+        "/events/{event_id}",
+        web::delete().to(delete_event_controller),
+    );
+    cfg.route(
+        "/events/{event_id}/instances",
+        web::get().to(get_event_instances_controller),
+    );
+}
