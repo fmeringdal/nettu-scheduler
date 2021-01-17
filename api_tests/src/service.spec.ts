57b3f1e9428c71b52b5e2c3fc8f09257feb982d6
Fredrik Meringdal <fmeringdal@hotmail.com>
2021-01-17 18:10:45 +0100

    fixes

--- api_tests/src/service.spec.ts
+++ api_tests/src/service.spec.ts
@@ -3,13 +3,19 @@ import { setupAccount } from "./helpers/fixtures";
 
 describe("Service API", () => {
   let client: INettuClient;
+  let accountId: string;
   beforeAll(async () => {
     const data = await setupAccount();
     client = data.client;
+    accountId = data.accountId;
   });
 
   it("should create service", async () => {
     const res = await client.service.insert();
     expect(res.status).toBe(201);
+
+    const serviceRes = await client.service.find(res.data.serviceId);
+    expect(serviceRes.data.id).toBe(res.data.serviceId);
+    expect(serviceRes.data.accountId).toBe(accountId);
   });
 });
