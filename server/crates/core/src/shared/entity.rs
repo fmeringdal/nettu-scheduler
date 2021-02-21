4efa99f54c71987fc1333247c80592badbc03960
Fredrik Meringdal <fredrikmeringdal@Fredriks-MacBook-Pro.local>
2021-02-21 18:59:37 +0100

    refactoring

--- server/crates/core/src/shared/entity.rs
+++ server/crates/core/src/shared/entity.rs
@@ -1,3 +1,7 @@
+// Also require Eq ?
 pub trait Entity {
     fn id(&self) -> String;
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
 }
--- server/crates/infra/src/account/mod.rs
+++ server/crates/infra/src/account/mod.rs
@@ -17,3 +17,22 @@ pub trait IAccountRepo: Send + Sync {
     async fn find_by_apikey(&self, api_key: &str) -> Option<Account>;
     async fn find_by_webhook_url(&self, url: &str) -> Option<Account>;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Context;
+    use nettu_scheduler_core::Entity;
+
+    fn get_ctx() -> Context {
+        Context::create_inmemory()
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        let ctx = get_ctx();
+        let account = Default::default();
+        assert!(ctx.repos.account_repo.insert(&account).await.is_ok());
+        let res = ctx.repos.account_repo.find(&account.id).await.unwrap();
+        assert!(res.eq(&account));
+    }
+}
