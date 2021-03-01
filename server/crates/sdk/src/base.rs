e32f048073a9e3c864359e4aafc7999a8d3fffb7
Fredrik Meringdal <fredrikmeringdal@Fredriks-MacBook-Pro.local>
2021-03-01 01:55:41 +0100

    clean up

--- server/crates/sdk/src/base.rs
+++ server/crates/sdk/src/base.rs
@@ -1,4 +1,4 @@
-use reqwest::{Client, Method, RequestBuilder, StatusCode};
+use reqwest::{Client, Method, RequestBuilder, Response, StatusCode};
 use serde::{Deserialize, Serialize};
 
 pub(crate) struct BaseClient {
@@ -10,6 +10,9 @@ pub(crate) struct BaseClient {
 pub enum APIError {
     Network,
     MalformedResponse,
+    Unauthorized,
+    Unauthenticated,
+    BadClientData,
     UnexpectedStatusCode(StatusCode),
 }
 pub type APIResponse<T> = Result<T, APIError>;
@@ -44,6 +47,36 @@ impl BaseClient {
         }
     }
 
+    fn check_status_code<T>(
+        &self,
+        res: &Response,
+        expected_status_code: StatusCode,
+    ) -> Result<(), APIError> {
+        let status = res.status();
+        if status != expected_status_code {
+            return Err(APIError::UnexpectedStatusCode(status));
+        }
+        Ok(())
+    }
+
+    async fn get_json_response<T: for<'de> Deserialize<'de>>(
+        &self,
+        res: Response,
+    ) -> APIResponse<T> {
+        res.json::<T>()
+            .await
+            .map_err(|_| APIError::MalformedResponse)
+    }
+
+    async fn handle_api_response<T: for<'de> Deserialize<'de>>(
+        &self,
+        res: Response,
+        expected_status_code: StatusCode,
+    ) -> APIResponse<T> {
+        self.check_status_code::<T>(&res, expected_status_code)?;
+        self.get_json_response(res).await
+    }
+
     pub async fn get<T: for<'de> Deserialize<'de>>(
         &self,
         path: String,
@@ -53,18 +86,32 @@ impl BaseClient {
             Ok(res) => res,
             Err(_) => return Err(APIError::Network),
         };
+        self.handle_api_response(res, expected_status_code).await
+    }
 
-        let status = res.status();
-        if status != expected_status_code {
-            return Err(APIError::UnexpectedStatusCode(status));
-        }
-
-        let data = match res.json::<T>().await {
-            Ok(data) => data,
-            Err(_) => return Err(APIError::MalformedResponse),
+    pub async fn delete<T: for<'de> Deserialize<'de>>(
+        &self,
+        path: String,
+        expected_status_code: StatusCode,
+    ) -> APIResponse<T> {
+        let res = match self.get_client(Method::DELETE, path).send().await {
+            Ok(res) => res,
+            Err(_) => return Err(APIError::Network),
         };
+        self.handle_api_response(res, expected_status_code).await
+    }
 
-        Ok(data)
+    pub async fn put<T: for<'de> Deserialize<'de>, S: Serialize>(
+        &self,
+        body: S,
+        path: String,
+        expected_status_code: StatusCode,
+    ) -> APIResponse<T> {
+        let res = match self.get_client(Method::PUT, path).json(&body).send().await {
+            Ok(res) => res,
+            Err(_) => return Err(APIError::Network),
+        };
+        self.handle_api_response(res, expected_status_code).await
     }
 
     pub async fn post<T: for<'de> Deserialize<'de>, S: Serialize>(
@@ -77,17 +124,6 @@ impl BaseClient {
             Ok(res) => res,
             Err(_) => return Err(APIError::Network),
         };
-
-        let status = res.status();
-        if status != expected_status_code {
-            return Err(APIError::UnexpectedStatusCode(status));
-        }
-
-        let data = match res.json::<T>().await {
-            Ok(data) => data,
-            Err(_) => return Err(APIError::MalformedResponse),
-        };
-
-        Ok(data)
+        self.handle_api_response(res, expected_status_code).await
     }
 }
