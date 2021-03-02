b3405b9a3de7e73ef0f876fb1639a0f0eb7ed8d5
Fredrik Meringdal <fredrikmeringdal@Fredriks-MacBook-Pro.local>
2021-03-02 23:04:14 +0100

    cleanup

--- server/tests/api.rs
+++ server/tests/api.rs
@@ -47,35 +47,12 @@ async fn test_create_user() {
 
     let account = res.account;
     let admin_client = NettuSDK::new(address, res.secret_api_key);
-    let user_id = "4124212421421";
     let res = admin_client
         .user
-        .create(user_id.into())
+        .create()
         .await
         .expect("Expected to create user");
-    assert_eq!(res.user.id, user_id);
     assert_eq!(res.user.account_id, account.id);
-}
-
-#[actix_web::main]
-#[test]
-async fn test_create_duplicate_user_should_not_work() {
-    let (app, sdk, address) = spawn_app().await;
-    let res = sdk
-        .account
-        .create(&app.config.create_account_secret_code)
-        .await
-        .expect("Expected to create account");
-
-    let admin_client = NettuSDK::new(address, res.secret_api_key);
-    let user_id = "4124212421421";
-    admin_client
-        .user
-        .create(user_id.into())
-        .await
-        .expect("Expected to create user");
-    let res = admin_client.user.create(user_id.into()).await;
-    assert!(res.is_err());
 }
 
 #[actix_web::main]
@@ -89,20 +66,19 @@ async fn test_create_schedule() {
         .expect("Expected to create account");
 
     let admin_client = NettuSDK::new(address, res.secret_api_key);
-    let user_id = "4124212421421";
-    admin_client
+    let create_user_res = admin_client
         .user
-        .create(user_id.into())
+        .create()
         .await
         .expect("Expected to create user");
     let res = admin_client
         .schedule
         .create(CreateScheduleInput {
-            user_id: user_id.into(),
+            user_id: create_user_res.user.id.clone(),
             timezone: "UTC".into(),
         })
         .await
         .expect("Expected to create schedule");
-    assert_eq!(res.schedule.user_id, user_id);
+    assert_eq!(res.schedule.user_id, create_user_res.user.id);
     assert_eq!(res.schedule.timezone, "UTC");
 }
