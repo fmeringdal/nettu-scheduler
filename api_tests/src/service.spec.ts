8e6e2722643f02e905ce0db26c349ca2bc1e05d6
Fredrik Meringdal <fredrikmeringdal@Fredriks-MacBook-Pro.local>
2021-04-01 11:40:58 +0200

    service api test

--- api_tests/src/service.spec.ts
+++ api_tests/src/service.spec.ts
@@ -105,7 +105,6 @@ describe("Service API", () => {
     });
     const now = new Date();
     const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-    console.log({ today });
 
     const { data } = await client.service.getBookingslots(serviceId, {
       date: today,
